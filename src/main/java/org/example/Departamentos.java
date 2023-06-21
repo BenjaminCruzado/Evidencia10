0ab0435eb76dd0c73180d650b6588cab071d1931
BenjaminCruzado <b.cruzado01@ufromail.cl>
2023-06-21 18:20:52 -0400

    Subo clases generados con el visual

---
+++ src/main/java/org/example/Departamentos.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+package org.example;
+
+public class Departamentos {
+	private String nombre;
+	private Empresa empresa;
+	private ArrayList<GrupoDeProyecto> grupos_de_proyecto = new ArrayList<GrupoDeProyecto>();
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
